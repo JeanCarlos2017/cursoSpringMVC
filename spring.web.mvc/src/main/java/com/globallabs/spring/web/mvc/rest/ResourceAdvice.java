f57e8b82801c422836cff26f29cbdea5a99f7b7e
JeanCarlos2017 <jean__100limites@hotmail.com>
2020-08-20 18:40:52 -0300

    Update ResourceAdvice.java
    consertando o retorno dos métodos

--- spring.web.mvc/src/main/java/com/globallabs/spring/web/mvc/rest/ResourceAdvice.java
+++ spring.web.mvc/src/main/java/com/globallabs/spring/web/mvc/rest/ResourceAdvice.java
@@ -1,16 +1,26 @@
 package com.globallabs.spring.web.mvc.rest;
 
-import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class ResourceAdvice {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(IndexOutOfBoundsException.class)
-    public JSONPObject indexOutofBounds(){
-        return new JSONPObject("Erro", "Indice nao encontrado");
+    public ResponseEntity<Map<String, Object>> indexOutofBounds( ){
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("message", "Index not found");
+        body.put("Retorne para :: ", "localhost8080/jedi");
+        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
     }
+
+
 }
